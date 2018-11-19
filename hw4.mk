218d032af6487e4b707e3e02a71c86f2db5cc845
Felipe   Bonilla Walker <UANDES\f.bonilla11@fisicaez12205l.ad.uniandes.edu.co>
2018-11-19 14:56:42 -0500

    Funciona todomake -f hw4.mk

--- hw4.mk
+++ hw4.mk
@@ -1,4 +1,7 @@
-all: projectile45.png projectiles7angulos.png rock0.png rock1.png rock2.png rockf.png rockm.png
+all:Resultados_hw4.pdf
+
+Resultados_hw4.pdf:projectile45.png projectiles7angulos.png rock0.png rock1.png rock2.png rockf.png rockm.png Resultados_hw4.tex
+	pdflatex Resultados_hw4.tex
 
 rock0.png rock1.png rock2.png rockf.png rockm.png: rock.csv rock1.csv rock2.csv Plots_hw4.py
 	python Plots_hw4.py PDE
@@ -21,3 +24,5 @@ clean:
 	rm *.png
 	rm *.csv
 	rm *.x
+
+
