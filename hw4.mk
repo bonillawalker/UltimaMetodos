ec6d07b99e285023dde2ae8bc19126cb86985b41
Felipe   Bonilla Walker <UANDES\f.bonilla11@fisicaez12205l.ad.uniandes.edu.co>
2018-11-19 12:41:14 -0500

    Este es mi make pero le deje los nombres de los archivos como lo voy a mandar en sicua

---
+++ hw4.mk
@@ -0,0 +1,23 @@
+all: projectile45.png projectiles7angulos.png rock0.png rock1.png rock2.png rockf.png rockm.png
+
+rock0.png rock1.png rock2.png rockf.png rockm.png: rock.csv rock1.csv rock2.csv Plots_hw4.py
+	python Plots_hw4.py PDE
+
+
+projectile45.png projectiles7angulos.png: projectile.csv projectile_7alpha.csv
+	python Plots_hw4.py ODE
+
+
+rock.csv rock1.csv rock2.csv: PDE.cpp
+	g++ PDE.cpp -o PDE.x
+	./PDE.x
+
+projectile.csv projectile_7alpha.csv: ODE.cpp
+	g++ ODE.cpp -o ODE.x
+	./ODE.x
+
+
+clean:
+	rm *.png
+	rm *.csv
+	rm *.x
